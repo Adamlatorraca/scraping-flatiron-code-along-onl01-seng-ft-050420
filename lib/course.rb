160af5dfcc0053f0ac5c296ecf76b5130f028efb
Adam LaTorraca <adam.latorraca@gmail.com>
2020-05-20 21:07:01 +0000

    Automatically backed up by Learn

--- lib/course.rb
+++ lib/course.rb
@@ -1,5 +1,6 @@
 class Course
   attr_accessor :title, :schedule, :description
+  @@all = []
 
   def initialize
     @@all << self
@@ -12,5 +13,5 @@ class Course
   def self.reset_all
     @@all.clear
   end
-  
+
 end
