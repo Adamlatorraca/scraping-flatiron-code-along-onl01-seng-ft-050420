6e14763cee0ed771ea7537b41c9d6117ad878d5c
Adam LaTorraca <adam.latorraca@gmail.com>
2020-05-20 21:04:39 +0000

    Automatically backed up by Learn

--- lib/course.rb
+++ lib/course.rb
@@ -1,2 +1,9 @@
 class Course
+  attr_accessor :title, :schedule, :description
+
+  def initialize(title, schedule, description)
+    @title = title
+    @schedukle = schedule
+    @description = description
+  end
 end
